57bd45912749fa02f8f10c2ff2b6bf601235ad7f
Lihini Gamage <126759357+LbGamage@users.noreply.github.com>
2025-05-31 16:30:44 +1000

    bug fixes and changing some stuff

--- glitchscript.js
+++ glitchscript.js
@@ -2,15 +2,15 @@ let wordChangeCount = 1; // initial number of words to change
 const wordChangeMax = 10; // maximum number of words that can be swapped
 const wordChangeStep = 1;
 
-const paragraphs = document.querySelectorAll('span'); // get all <p> elements
+const paragraphs = document.querySelectorAll('span'); 
 const originals = []; // store original text for each paragraph
-const wordsArray = []; // store words array for each paragraph
+const wordsArray = []; 
 
 const alternateWords = [
   "heRe", "here", "her", "he's coming", "mike", "  ", "Look here!", "HeLp", "Bsnefwe", "efioefjoewif", "efieff", "hidden", "look", "LOOK", "I", "I AM", "I", "I", "console", "Help", "heLPkef"
 ];
 
-// Initialize original texts and split words
+
 paragraphs.forEach((p, idx) => {
   originals[idx] = p.textContent;
   wordsArray[idx] = originals[idx].split(' ');
@@ -41,3 +41,4 @@ function swapWords() {
 setTimeout(() => {
   setInterval(swapWords, 800);
 }, 5000);
+
